77dc3fa86aa0124147e7fd1bb9222c008122b70c
kritoth <kritoth@gmail.com>
2020-09-22 18:38:58 +0200

    Change to view binding

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.RequestConfiguration;
+import com.udacity.gradle.builditbigger.databinding.FragmentMainBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +22,22 @@ import java.util.List;
  */
 public class MainActivityFragment extends Fragment {
 
+    private FragmentMainBinding binding;
+    private TextView mJokeView;
+    private AdView mAdView;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        mJokeView = binding.instructionsTextView;
+
+        mAdView = binding.adView;
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device." -DEPRECATED
