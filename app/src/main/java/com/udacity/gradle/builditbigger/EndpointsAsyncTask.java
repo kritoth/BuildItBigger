61c0dc4bd40f9293f00a3b76826e6b4337ef4571
TianSirk <tiansirk@outlook.hu>
2020-09-24 11:29:12 +0200

    Solve "Cleartext HTTP traffic not permitted" error in a DIFFERENT way to let 'http' untouched in order to avoid SSLHandshake error, which is far more difficult to solve. The solution is to add usesCleartextTraffic tag and set it to true. See: https://stackoverflow.com/questions/45940861/android-8-cleartext-http-traffic-not-permitted

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -35,7 +35,7 @@ public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
                     //.setApplicationName(context.getString(R.string.app_name))
-                    .setRootUrl("https://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
